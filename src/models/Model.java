c90d653a34ddc146e8454347b03330a37be1fa74
joao <jgerhardt923@gmail.com>
2022-01-24 11:23:13 -0400

    Model class created

---
+++ src/models/Model.java
@@ -0,0 +1,150 @@
+package src.models;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Model {
+    String tableName;
+    Connection connection;
+
+    /**
+     * retorna uma string com os dados do <b>hashMap</b>
+     * @param hashMap
+     * HashMap contendo os dados
+     * @param separator
+     * String para separar os dados
+     * @param mode
+     * <p>string representando o modo como a string sera retornada, pode ser:</p>
+     * <p><b>k</b> | <b>key</b> para retornar apenas as chaves do hashMap</p>
+     * <p><b>v</b> | <b>value</b> para retornar apenas os valores do hashMap</p>
+     * <p><b>k=v</b> | <b>key=value</b> para retornar os pares <b>chave=valor</b> do hashMap</p>
+     * @return
+     * <b>String</b> ou <b>null</b>
+     */
+    public String stringFromHashMap(HashMap<String, Object> hashMap, String separator, String mode) {
+        String string = "";
+        if (mode == "k" | mode == "key") {
+            for (String keyString : hashMap.keySet()) {
+                string += keyString +" "+ separator + " ";
+            }
+            return string.substring(0, string.length() - (separator.length() + 1));
+        }
+        if (mode == "v" | mode == "value") {
+            for (String keyString : hashMap.keySet()) {
+                string += "'" + String.valueOf(hashMap.get(keyString)) +"' "+ separator + " ";
+            }
+            return string.substring(0, string.length() - (separator.length() + 1));
+        }
+        if (mode == "k=v" | mode == "key=value") {
+            for (String keyString : hashMap.keySet()) {
+                string += keyString + "='" + String.valueOf(hashMap.get(keyString)) +"' "+ separator + " ";
+            }
+            return string.substring(0, string.length() - (separator.length() + 1));
+        }
+        return null;
+    }
+
+    /**
+     * Retorna um ArrayList dos dados do rSet
+     * @param rSet
+     *      ResultSet retornado da query
+     * @return
+     *      ArrayList com HashMaps contendo os dados da query
+     */
+    public ArrayList<HashMap<String, Object>> hashMapFromTable(ResultSet rSet) {
+        try {
+            ArrayList<HashMap<String, Object>> rows = new ArrayList<>();
+            ResultSetMetaData metaData = rSet.getMetaData();
+            int nColunas = metaData.getColumnCount();
+
+            while (rSet.next()) {
+                HashMap<String, Object> row = new HashMap<>();
+                for (int i = 1; i <= nColunas; i++) {
+                    row.put(metaData.getColumnName(i), rSet.getObject(i));
+                }
+                rows.add(row);
+            }
+
+            return rows;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public void create(HashMap<String, Object> data){
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("insert into "+ tableName +"("+ stringFromHashMap(data, ",", "k") +") values("+ stringFromHashMap(data, ",", "v") +");");
+            statement.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void delete(Integer id){
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("delete from "+ tableName +" where id="+String.valueOf(id)+";");
+            statement.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void update(Integer id, HashMap<String, Object> data){
+        String kString = stringFromHashMap(data, ",", "k=v");
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("update "+ tableName +" set "+kString+" where id="+id+";");
+            statement.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public HashMap<String, Object> getOne(HashMap<String, Object> filter){
+        String kString = stringFromHashMap(filter, "and", "k=v");
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from "+ tableName +" where "+kString+" limit 1;");
+            ArrayList<HashMap<String, Object>> res =  hashMapFromTable(resultSet);
+            statement.close();
+            return res.get(0);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public ArrayList<HashMap<String, Object>> getAll() {
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from "+ tableName +";");
+            ArrayList<HashMap<String, Object>> res =  hashMapFromTable(resultSet);
+            statement.close();
+            return res;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public ArrayList<HashMap<String, Object>> getAll(HashMap<String, Object> filter) {
+        String kString = stringFromHashMap(filter, "and", "k=v");
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from "+ tableName +" where "+kString+" ;");
+            ArrayList<HashMap<String, Object>> res =  hashMapFromTable(resultSet);
+            statement.close();
+            return res;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
--- src/models/Usuario.java
+++ src/models/Usuario.java
@@ -1,101 +1,10 @@
 package src.models;
 
 import java.sql.Connection;
-import java.sql.Statement;
-import java.util.HashMap;
 
-public class Usuario {
-    Connection connection;
-
+public class Usuario extends Model {
     public Usuario(Connection connection) {
         this.connection = connection;
-    }
-
-    /**
-     * retorna uma string com os dados do <b>hashMap</b>
-     * @param hashMap
-     * HashMap contendo os dados
-     * @param separator
-     * String para separar os dados
-     * @param mode
-     * <p>string representando o modo como a string sera retornada, pode ser:</p>
-     * <p><b>k</b> | <b>key</b> para retornar apenas as chaves do hashMap</p>
-     * <p><b>v</b> | <b>value</b> para retornar apenas os valores do hashMap</p>
-     * <p><b>k=v</b> | <b>key=value</b> para retornar os pares <b>chave=valor</b> do hashMap</p>
-     * @return
-     * <b>String</b> ou <b>null</b>
-     */
-    public String stringFromHashMap(HashMap<String, Object> hashMap, String separator, String mode) {
-        String string = "";
-        if (mode == "k" | mode == "key") {
-            for (String keyString : hashMap.keySet()) {
-                string += keyString +" "+ separator + " ";
-            }
-            return string.substring(0, (-1 * separator.length())-1);
-        }
-        if (mode == "v" | mode == "value") {
-            for (String keyString : hashMap.keySet()) {
-                string += "'" + String.valueOf(hashMap.get(keyString)) +"' "+ separator + " ";
-            }
-            return string.substring(0, (-1 * separator.length())-1);
-        }
-        if (mode == "k=v" | mode == "key=value") {
-            for (String keyString : hashMap.keySet()) {
-                string += keyString + "='" + String.valueOf(hashMap.get(keyString)) +"' "+ separator + " ";
-            }
-            return string.substring(0, (-1 * separator.length())-1);
-        }
-        return null;
-    }
-
-    /**
-     * Insere um novo objeto no banco de dados
-     * @param data
-     * deve receber um HashMap onde <b>key</b> é o nome do campo e <b>value</b> é o valor do campo 
-     */
-    public void create(HashMap<String, Object> data){
-        try {
-            Statement statement = connection.createStatement();
-            statement.execute("insert into Usuario("+ stringFromHashMap(data, ",", "k") +") values("+ stringFromHashMap(data, ",", "v") +");");
-            System.out.println(data.get("nome")+" registrado");
-            statement.close();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public void delete(Integer id){
-        try {
-            Statement statement = connection.createStatement();
-            statement.execute("delete from Usuario where id="+String.valueOf(id)+";");
-            System.out.println("usuario "+id+" apagado!");
-            statement.close();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public void update(Integer id, HashMap<String, Object> data){
-        String kString = stringFromHashMap(data, ",", "k=v");
-        try {
-            Statement statement = connection.createStatement();
-            statement.execute("update Usuario set "+kString+" where id="+id+";");
-            System.out.println("usuario "+id+" atualizado!");
-            statement.close();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public void getOne(HashMap<String, Object> data){  
-
-    }
-
-    public void getAll() {
-        
-    }
-
-    public void getAll(HashMap<String, Object> data) {
-        
+        this.tableName = "Usuario";
     }
 }
