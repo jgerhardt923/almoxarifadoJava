32ec854bba1d339460f6d20fb8127bc2921589e5
joao <jgerhardt923@gmail.com>
2022-02-14 09:00:01 -0400

    movimentacao class created

--- src/Almoxarifado.java
+++ src/Almoxarifado.java
@@ -82,30 +82,32 @@ public class Almoxarifado{
 
     private static void printMenuOptions() {
         console.printf("#Almoxerifado app\n\n");
-        console.printf("    Usuarios\n");
-        console.printf("        |__  novo_______________[11]\n");
-        console.printf("        |__  listar_____________[12]\n");
-        console.printf("        |__  atualizar__________[13]\n");
-        console.printf("        |__  excluir____________[14]\n\n");
+        /*if (usuarioLogado.get("adm").equals(Integer.parseInt("1"))){
+            console.printf("    Usuarios\n");
+            console.printf("        |__  novo_______________[11]\n");
+            console.printf("        |__  listar_____________[12]\n");
+            console.printf("        |__  atualizar__________[13]\n");
+            console.printf("        |__  excluir____________[14]\n\n");
+        }*/
         console.printf("    Categorias\n");
         console.printf("        |__  novo_______________[21]\n");
         console.printf("        |__  listar_____________[22]\n");
         console.printf("        |__  atualizar__________[23]\n");
-        console.printf("        |__  excluir____________[24]\n\n");
+        //if (usuarioLogado.get("adm").equals(Integer.parseInt("1"))) console.printf("        |__  excluir____________[24]\n\n");
         console.printf("    Objetos\n");
         console.printf("        |__  novo_______________[31]\n");
         console.printf("        |__  listar_____________[32]\n");
         console.printf("        |__  atualizar__________[33]\n");
-        console.printf("        |__  excluir____________[34]\n\n");
+        //if (usuarioLogado.get("adm").equals(Integer.parseInt("1"))) console.printf("        |__  excluir____________[34]\n\n");
         console.printf("    Movimentacoes\n");
         console.printf("        |__  Entradas\n");
         console.printf("        |       |__  novo_______[41]\n");
         console.printf("        |       |__  listar_____[42]\n");
-        console.printf("        |       |__  excluir____[43]\n\n");
+        //if (usuarioLogado.get("adm").equals(Integer.parseInt("1"))) console.printf("        |       |__  excluir____[43]\n\n");
         console.printf("        |__  Saidas\n");
-        console.printf("                |__  novo_______[51]\n");
+        console.printf("        |       |__  novo_______[51]\n");
         console.printf("        |       |__  listar_____[52]\n");
-        console.printf("                |__  excluir____[53]\n\n\n");
+        //if (usuarioLogado.get("adm").equals(Integer.parseInt("1"))) console.printf("        |       |__  excluir____[53]\n\n\n");
         console.printf("    Opcoes\n");
         console.printf("        |__  mostrar menu_______[61]\n");
         console.printf("        |__  sair_______________[62]\n");
@@ -116,19 +118,19 @@ public class Almoxarifado{
     }
 
     private static boolean processOption(String option) {
-        if (option.equals("11")) {
+        /*if (option.equals("11") & usuarioLogado.get("adm").equals(Integer.parseInt("1"))) {
             usuarioView.create();
             return true;
-        } else if (option.equals("12")) {
+        } else if (option.equals("12") & usuarioLogado.get("adm").equals(Integer.parseInt("1"))) {
             usuarioView.list();
             return true;
-        } else if (option.equals("13")) {
+        } else if (option.equals("13") & usuarioLogado.get("adm").equals(Integer.parseInt("1"))) {
             usuarioView.update();
             return true;
-        } else if (option.equals("14")) {
+        } else if (option.equals("14") & usuarioLogado.get("adm").equals(Integer.parseInt("1"))) {
             usuarioView.delete();
             return true;
-        } else if (option.equals("21")) {
+        } else */if (option.equals("21")) {
             categoriaView.create();
             return true;
         } else if (option.equals("22")) {
@@ -137,10 +139,10 @@ public class Almoxarifado{
         } else if (option.equals("23")) {
             categoriaView.update();
             return true;
-        } else if (option.equals("24")) {
+        }/* else if (option.equals("24") & usuarioLogado.get("adm").equals(Integer.parseInt("1"))) {
             categoriaView.delete();
             return true;
-        } else if (option.equals("31")) {
+        }*/ else if (option.equals("31")) {
             objetoView.create();
             return true;
         } else if (option.equals("32")) {
@@ -149,22 +151,22 @@ public class Almoxarifado{
         } else if (option.equals("33")) {
             objetoView.update();
             return true;
-        } else if (option.equals("34")) {
+        }/* else if (option.equals("34") & usuarioLogado.get("adm").equals(Integer.parseInt("1"))) {
             objetoView.delete();
             return true;
-        } else if (option.equals("41")) {
+        }*/ else if (option.equals("41")) {
             return true;
         } else if (option.equals("42")) {
             return true;
-        } else if (option.equals("43")) {
+        }/* else if (option.equals("43") & usuarioLogado.get("adm").equals(Integer.parseInt("1"))) {
             return true;
-        } else if (option.equals("51")) {
+        }*/ else if (option.equals("51")) {
             return true;
         } else if (option.equals("52")) {
             return true;
-        } else if (option.equals("53")) {
+        }/* else if (option.equals("53") & usuarioLogado.get("adm").equals(Integer.parseInt("1"))) {
             return true;
-        } else if (option.equals("61")) {
+        }*/ else if (option.equals("61")) {
             printMenuOptions();
             return true;
         } else if (option.equals("62")) {
---
+++ src/views/Movimentacao.java
@@ -0,0 +1,30 @@
+package src.views;
+
+import java.io.Console;
+import java.util.HashMap;
+
+import src.models.Model;
+
+public class Movimentacao {
+    private Model model;
+    private Console console;
+
+    public void createEntrada() {
+        /*try {
+            HashMap<String, Object> data = new HashMap<>();
+            data.put("", console.readLine(""+": "));
+            model.create(data);
+            console.format("criado!\n");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }*/
+    }
+
+    public void listEntrada() {
+        
+    }
+
+    public void deleteEntrada() {
+        
+    }
+}
